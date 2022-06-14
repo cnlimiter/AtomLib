eac2c6b49dba7afa8923ffa992b46045e9d51597
cnlimiter <cnlimiter@163.com>
2022-06-14 19:47:28 +0800

    update 2

---
+++ src/main/java/cn/evolvefield/mods/atomlib/client/hud/AbstractBarOverlay.java
@@ -0,0 +1,19 @@
+package cn.evolvefield.mods.atomlib.client.hud;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.world.entity.player.Player;
+
+/**
+ * Description:
+ * Author: cnlimiter
+ * Date: 2022/3/30 12:07
+ * Version: 1.0
+ */
+public abstract class AbstractBarOverlay implements IBarOverlay {
+
+    public void renderAll(Player player, PoseStack poseStack) {
+        this.renderBar(player, poseStack);
+        this.renderText(player, poseStack);
+        this.renderIcon(player, poseStack);
+    }
+}
---
+++ src/main/java/cn/evolvefield/mods/atomlib/utils/FileUtil.java
@@ -0,0 +1,42 @@
+package cn.evolvefield.mods.atomlib.utils;
+
+
+import cn.evolvefield.mods.atomlib.AtomLib;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Description:
+ * Author: cnlimiter
+ * Date: 2022/1/1 15:30
+ * Version: 1.0
+ */
+public class FileUtil {
+
+    public static Path checkFolder(Path folder) {
+        if (!folder.toFile().isDirectory()) {
+            try {
+                return Files.createDirectories(folder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return folder;
+    }
+
+
+    public static void streamResourceToDisk(URL inputUrl, File filePath) throws IOException {
+        if (inputUrl == null) {
+            AtomLib.LOGGER.error("源文件夹是空的: " + filePath.toString());
+        } else {
+            FileUtils.copyURLToFile(inputUrl, filePath);
+        }
+    }
+
+
+}
